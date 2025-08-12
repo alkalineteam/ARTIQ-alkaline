96ac20b828f683d26f7c5d22a75abca2fcac40ca
Anurag Borah <anuragborah2548@gmail.com>
2025-08-12 11:56:13 +0100

    --

--- imports.py
+++ imports.py
@@ -8,15 +8,15 @@ import sklearn
 import requests
 # import boto3
 
-# t1 = torch.randn(10, 10)
-# t2 = torch.randn(10, 10)
+t1 = torch.randn(10, 10)
+t2 = torch.randn(10, 10)
 
-# @torch.compile
-# def opt_foo2(x, y):
-#     a = torch.sin(x)
-#     b = torch.cos(y)
-#     return a + b
-# print(opt_foo2(t1, t2))
+@torch.compile
+def opt_foo2(x, y):
+    a = torch.sin(x)
+    b = torch.cos(y)
+    return a + b
+print(opt_foo2(t1, t2))
 
 print(torch.__version__)
 print(torch.tensor([1, 2, 3]))
