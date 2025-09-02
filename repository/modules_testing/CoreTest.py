a45b1baab2487da9b0bb93e9e28cb87a920cb2dc
Anurag Borah <anuragborah2548@gmail.com>
2025-09-02 17:03:43 +0100

    added torch support

--- repository/modules_testing/CoreTest.py
+++ repository/modules_testing/CoreTest.py
@@ -1,13 +1,22 @@
+import torch
 from artiq.experiment import *
-from artiq.coredevice.core import Core
+from ndscan.experiment import Fragment
+from numpy import int64, int64
 
 class TestCore(EnvExperiment):
-    def build(self):
-        self.setattr_device("core")
-        self.core:Core
+	def build(self):
+		self.setattr_device("core")
 
-    @kernel
-    def run(self):
-        self.core.reset()
+	@host_only
+	def run(self):
+		print("Hello testbed setup")
 
-        print("Core tested successfully")
+		t1 = torch.randn(2, 2)
+		t2 = torch.randn(2, 2)
+
+		@torch.compile
+		def opt_foo2(x, y):
+			a = torch.sin(x)
+			b = torch.cos(y)
+			return a + b
+		print(opt_foo2(t1, t2))
