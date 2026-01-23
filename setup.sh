3ca21100f300610454b373c55b1a3e7191d0e75b
Anurag Borah <anuragborah2548@gmail.com>
2026-01-23 13:02:31 +0000

    updated to write to apt sources

--- setup.sh
+++ setup.sh
@@ -51,10 +51,12 @@ if ! command -v docker &> /dev/null; then
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null
+    echo "Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: noble
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc" | \
+    sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null
 
     sudo apt update
     sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
